56429e29ee503b108a21a5081e6119b266c77513
Jsingh651 <jangsing02@gmail.com>
2023-04-16 20:14:32 -0700

    d

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
-console.log(process.env.OPENAI_API_KEY)
+
 
 const openai = new OpenAIApi(config);
 
