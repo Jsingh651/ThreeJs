e3f57f15c4c4796da7ef1ae2411d0d76b9a21455
Jsingh651 <jangsing02@gmail.com>
2023-04-16 20:46:29 -0700

    f

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import CustomButton from './CustomButton';
 
-const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
+const AIPicker = () => {
 return(
   <div>
   </div>
